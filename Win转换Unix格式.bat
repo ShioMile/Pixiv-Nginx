f8ad100d194d55cc0ee2009eb5fd00721f208825
ShioMile <gra4drog666@protonmail.com>
2021-01-05 23:30:57 +0800

    fix & simplify dos2unix batchfile

--- Win转换Unix格式.bat
+++ Win转换Unix格式.bat
@@ -2,8 +2,7 @@
 title 换行符转换Windows→Unix（CRLF → LF）
 echo 适用于类Unix系统（如Android）的换行符转换工具
 pause
-dos2unix hosts hosts
+dos2unix hosts
 cd /d conf/
-dos2unix nginx.conf nginx.conf
-dos2unix pixiv.conf pixiv.conf
+dos2unix *.conf
 pause
