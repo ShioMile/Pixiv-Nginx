630e64f563da9de513c22aa0640e21d49346682a
ShioMile <gra4drog666@protonmail.com>
2021-01-18 21:03:10 +0800

    add vimeo.com

--- Win转换Unix格式.bat
+++ Win转换Unix格式.bat
@@ -1,8 +1,9 @@
 @echo off
 title 换行符转换Windows→Unix（CRLF → LF）
-echo 适用于类Unix系统（如Android）的换行符转换工具
+echo 适用于类Unix系统（如Android）的换行符转换工具，Windows勿误用！
 pause
 dos2unix hosts
 cd /d conf/
-dos2unix *.conf
+dos2unix nginx.conf
+dos2unix pixiv.conf
 pause
